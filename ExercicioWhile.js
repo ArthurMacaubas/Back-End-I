aebf1a6d871c3c80fb866f9140ebb20630328a54
Arthur Pietro <arthurpietro847@gmail.com>
2025-05-26 13:57:49 -0400

    26/05/25

--- ExercicioWhile.js
+++ ExercicioWhile.js
@@ -7,22 +7,51 @@ let i = 0
 while (true) {
     console.log(`${num} x ${i} = ${num * i}`)
     i = i + 1
-    if (i === 11) {
+    if (i === 10) {
         break
     }
 }
 
 // Atividade 2
-let n =0
+let numTurma = input.questionInt("Número de alunos: ");
+let notas = [[], [], [], []];
+let mediasBim = [];
+let mediasAnuais = [];
+let somaTotalNotas = 0;
+let aluno = 1;
 
-let numTurma = input.questionInt("Insira o número de alunos na turma: ")
-
-while (true){
-    n = n + 1
-    let notaAlunos = input.question(`Insira a nota do ${n}° aluno: `)
-    let mediaTurma = notaAlunos / numTurma
-    if (n === numTurma) {
-        console.log(mediaTurma)
-        break
+while (aluno <= numTurma) {
+    let notasAluno = [];
+    let bimestre = 0;
+    while (bimestre < 4) {
+        let nota = input.questionFloat(`Nota do aluno ${aluno}, bimestre ${bimestre + 1}: `);
+        notas[bimestre].push(nota);
+        notasAluno.push(nota);
+        somaTotalNotas += nota;
+        bimestre++;
     }
+    mediasAnuais.push(notasAluno.reduce((soma, n) => soma + n, 0) / 4);
+    aluno++;
 }
+
+let bimestre = 0;
+while (bimestre < 4) {
+    mediasBim.push(notas[bimestre].reduce((soma, n) => soma + n, 0) / numTurma);
+    bimestre++;
+}
+
+let mediaTotal = somaTotalNotas / (numTurma * 4);
+
+console.log("\nMédias por bimestre:");
+bimestre = 0;
+while (bimestre < 4) {
+    console.log(`Bimestre ${bimestre + 1}: ${mediasBim[bimestre].toFixed(2)}`);
+    bimestre++;
+}
+console.log("\nMédias anuais:");
+aluno = 0;
+while (aluno < numTurma) {
+    console.log(`Aluno ${aluno + 1}: ${mediasAnuais[aluno].toFixed(2)}`);
+    aluno++;
+}
+console.log(`\nMédia total da turma: ${mediaTotal.toFixed(2)}`);
